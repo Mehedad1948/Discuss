f7d316c47fc4e4c47b5c17a20cf18e897d38ed98
Mehrdad Nouri <mnourib12@gmail.com>
2024-03-01 21:33:30 +0330

    Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import { Button } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
+import Profile from './components/profule';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click</Button>
+      <form action={actions.signIn}>
+        <Button type='submit'>Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type='submit'>Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Sign Out</div>
+      )}
+
+      <Profile />
     </div>
   );
 }
